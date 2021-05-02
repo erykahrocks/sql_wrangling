09b30d631c8a40cc68042d0a3131667b1ab13cef
erykahrocks <hagsfdf123@gmail.com>
2021-05-02 19:20:58 +0900

    minor add in custom error for validation

--- error.py
+++ error.py
@@ -1,6 +1,7 @@
-class RequiredInfoMissingError(Exception):
+class RequiredTableMissingError(Exception):
     """Raised when person or visit occurrence is missed from Note"""
     pass
+
 
 class DBInsertionError(Exception):
     """DB insertion failed with some fatal issues"""
@@ -8,4 +9,15 @@ class DBInsertionError(Exception):
 
 class NoteInvalidError(Exception):
     """Parsed note didn't pass validation"""
+    def __init__(self, message='Parsed note didn\'t pass validation'):
+        self.message = message
+        super().__init__(self.message)
+
+class RequiredInfoMissingError(Exception):
+    """Raised when required attribute is missed from Note"""
+
+    # provide with missing info in msg
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
     pass
